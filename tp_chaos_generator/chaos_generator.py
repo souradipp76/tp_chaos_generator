Corrected algorithms with half precision.

tp_chaos_generator/chaos_generator.py
@@ -1,21 +1,27 @@
+""" Chaos Generator """
+import struct
 import numpy as np
-
-import utils
 import triple_pendulum_ode
 
+from utils import circular_bit_rotate, half_precision
+
 class ChaosGenerator: 
+    """ Chaos Generator """
     def __init__(
         self,
         key: list
     ) -> None:
-        self.encryption_key = key
-        self.fps = 1000
+        self.key = key
+        self.fps = 10000
         self.tstart = 0
-        self.tend = 10
+        self.tend = 6.5535
         self.delta_t = 1./self.fps
+        self.num_chars = 256
+        self.eta = 0.9
 
     @classmethod
     def generate_key(cls) -> list:
+        """ Key Generation """
         m1=0.2944
         m2=0.1765
         m3=0.0947
@@ -30,7 +36,6 @@ class ChaosGenerator:
         I3=1.848e-4
         g=9.81
 
-
         theta1 = -0.4603
         theta2 = -1.2051
         theta3 = -1.5165
@@ -41,74 +46,125 @@ class ChaosGenerator:
         key = [theta1,theta2,theta3,dtheta1,dtheta2,dtheta3,m1,m2,m3,l1,l2,l3,I1,I2,I3,k1,k2,k3,g]
         return key
 
-    def encrypt(self, data: bytes) -> list:
-        ivp = self.encryption_key
+    def encrypt(self, plain_text: list) -> list:
+        """ Data Encryption """
+        _, yy = triple_pendulum_ode.triple_pendulum_ode(
+            self.tstart,
+            self.tend,
+            self.delta_t,
+            self.key)
 
-        num_chars = 256
-        eta = 0.9
-        
-        t, yy = triple_pendulum_ode.triple_pendulum_ode(self.tstart, self.tend, self.delta_t, ivp)
         Y = yy[2]
         y_min = np.min(Y)
         y_max = np.max(Y)
-        epsilon = (y_max - y_min)/num_chars
+        epsilon = (y_max + self.delta_t - y_min)/self.num_chars
         #epsilon = 2*np.pi/num_chars
-        
-        intervals = []
+
+        key_len = len(self.key)
+
+        ### generate lookup table ###
+        lookup_table = []
+        for i in range(self.num_chars):
+            lookup_table.append([])
+
+        for j, y_val in enumerate(Y):
+            d = int(np.floor((y_val - y_min)/epsilon))
+            lookup_values = lookup_table[d]
+            lookup_values.append(j)
+            lookup_table[d] = lookup_values
+
+        # print("Lookup Table")
+        # print(lookup_table)
+
+        ### Encryption ###
         y = []
-        
-        print(y_min, y_max)
-        for i in range(num_chars-1):
-            possibles = []
-            for j,z in enumerate(Y):
-                if  (z >= y_min+i*epsilon) and (z < y_min + (i+1)*epsilon):
-                    possibles.append(j)
 
-            if len(possibles) == 0:
-                print(f"{i}\n")
-            intervals.append(possibles)
+        ### selecting f(self.key) ###
+        # f = halfprecision(self.key);
+        # for K in range(1, key_len):
+        #     f = bitxor(f, halfprecision(self.key[K]));
 
-        plain_text_arr = data
-        for k in range(len(data)):
-            d = plain_text_arr[k]
+        for i, p in enumerate(plain_text):
             flag = False
             index = 0
             while flag is False:
-                r = np.random.randn()
-                c = intervals[d]
-                if r > eta:
-                    y.append(c[index])
+                r = np.random.rand()
+                #print(f"Random num generated: {r}")
+                lookup_values = lookup_table[p]
+                #c = intervals{1,d+1};
+
+                ### select random between 0 to 1 and compare with eta ###
+                if r > self.eta:
+                    #print(f"Index selected: {index}, Value: {lookup_values[index]}")
+                    C = lookup_values[index]
+
+                    f = struct.unpack('H', half_precision(self.key[i%key_len]))[0]
+
+                    ### Operations ###
+                    C = circular_bit_rotate(C, -(i%16), 16)
+                    C = C^f
+                    C = circular_bit_rotate(C, -(i%16), 16)
+                    C = C^f
+                    C = circular_bit_rotate(C, -(i%16), 16)
+                    C = C^f
+
+                    y.append(C)
                     flag = True
-                    index+=1
                 else:
-                    index = index % len(c)
+                    index = (index+1) % len(lookup_values)
+
+                # ### select random index ###
+                # len = len(lookup_values);
+                # r2 = np.random.randint([1, len])
+                # print(f"Random num generated for length {len}: {r2}")
+                # C = lookup_values(r2);
+                # print(f"Iteration: {index}: key: {f}");
+                # yy = bitxor(C, bitror(f, k));
+                # y.append(yy);
+                # flag= True
 
         return y
 
-    def decrypt(
-        self,
-        data
-    ) -> list:
+    def decrypt(self, cipher_text: list) -> list:
+        """ Data Decryption """
+        key_len = len(self.key)
 
-        ivp = self.encryption_key
-        num_chars = 256
-
-        t, yy = triple_pendulum_ode.triple_pendulum_ode(self.tstart, self.tend, self.delta_t, ivp)
+        _, yy = triple_pendulum_ode.triple_pendulum_ode(
+            self.tstart,
+            self.tend,
+            self.delta_t,
+            self.key)
         Y = yy[2]
         y_min = np.min(Y)
         y_max = np.max(Y)
-        epsilon = (y_max - y_min)/num_chars
-        
+        epsilon = (y_max - y_min)/self.num_chars
+
         y = []
-        cipher_text_arr = data
-        for x in cipher_text_arr:
-            y_val =  Y[x]
-            d = (y_val - y_min)/epsilon;
-            y.append(int(np.floor(d)));
+
+        ### selecting f(self.key) ###
+        # f = halfprecision(self.key);
+        # for K in range(1, key_len):
+        #     f = bitxor(f, halfprecision(self.key[K]));
+
+        for i, c in enumerate(cipher_text):
+            f = struct.unpack('H', half_precision(self.key[i%key_len]))[0]
+
+            ### Operations ###
+            C = c^f
+            C = circular_bit_rotate(C, i%16, 16)
+            C = C^f
+            C = circular_bit_rotate(C, i%16, 16)
+            C = C^f
+            C = circular_bit_rotate(C, i%16, 16)
+
+            y_val =  Y[C]
+            d = np.floor((y_val - y_min)/epsilon)
+            y.append(d)
 
         return y
 
 def main():
+    """ Main """
     m1=0.2944
     m2=0.1765
     m3=0.0947
@@ -123,7 +179,6 @@ def main():
     I3=1.848e-4
     g=9.81
 
-
     theta1 = -0.4603
     theta2 = -1.2051
     theta3 = -1.5165
@@ -136,12 +191,14 @@ def main():
 
     str = 'A course in Cryptography'
     plain_text = [ord(x) for x in str]
+    print("Plain Text: ", plain_text)
     cipher_text = cg.encrypt(plain_text)
+    print("Cipher Text: ", cipher_text)
     clear_text = cg.decrypt(cipher_text)
 
-    dec_text = [chr(i) for i in clear_text]
+    dec_text = [chr(int(i)) for i in clear_text]
     dec_text = ''.join(dec_text)
-    print(dec_text)
+    print("Clear Text: ", dec_text)
 
 if __name__ == "__main__":
     main()

tp_chaos_generator/triple_pendulum_ode.py
@@ -1,14 +1,17 @@
-import numpy as np
-from scipy.integrate import solve_ivp
+""" Utils """
 
-from utils import state_plotter, plot_state, plot_trajectory
+import numpy as np
+
+from scipy.integrate import solve_ivp
+from utils import state_plotter, trajectory_plotter
 
 def normalize(y):
+    """ Normalize States """
     y = y - np.floor(y/(2*np.pi))*(2*np.pi)
     return y
 
 def triple_pendulum_ode(start, end, step, ivp) -> list:
-    """ODE"""
+    """ Solve ODE """
     params = ivp[6:19]
     xinit = ivp[0:6]
 
@@ -21,7 +24,7 @@ def triple_pendulum_ode(start, end, step, ivp) -> list:
     return t,y
 
 def f(t, x, params) -> list:
-    """ ODE"""
+    """ ODE Function """
     m1, m2, m3, L1, L2, L3, I1, I2, I3, k1, k2, k3, g = params
     y0 = x[3]
     y1 = x[4]
@@ -34,19 +37,20 @@ def f(t, x, params) -> list:
     return dydt
 
 def main():
-    m1=0.2944
-    m2=0.1765
-    m3=0.0947
-    l1=0.508
-    l2=0.254
-    l3=0.127
-    k1=0.005
-    k2=0
-    k3=0.0008
-    I1=9.526e-3
-    I2=1.625e-3
-    I3=1.848e-4
-    g=9.81
+    """ Main Function """
+    m1 = 0.2944
+    m2 = 0.1765
+    m3 = 0.0947
+    l1 = 0.508
+    l2 = 0.254
+    l3 = 0.127
+    k1 = 0.005
+    k2 = 0
+    k3 = 0.0008
+    I1 = 9.526e-3
+    I2 = 1.625e-3
+    I3 = 1.848e-4
+    g = 9.81
 
 
     theta1 = -0.4603
@@ -56,17 +60,16 @@ def main():
     dtheta2 = 0
     dtheta3 = 0
 
-    key = [theta1,theta2,theta3,dtheta1,dtheta2,dtheta3,m1,m2,m3,l1,l2,l3,I1,I2,I3,k1,k2,k3,g]
+    key = [theta1, theta2, theta3, dtheta1, dtheta2, dtheta3, m1, m2, m3, l1, l2, l3, I1, I2, I3, k1, k2, k3, g]
 
     start = 0
     stop = 10
     fps = 10000
     delta_t=1.0/fps
     t, y = triple_pendulum_ode(start, stop, delta_t, key)
-    
-    #state_plotter(sol.t, sol.y, 1)
-    plot_state(t, y, 2)
-    # plot_trajectory(sol.t, sol.y, c)
+
+    state_plotter(t, y, 1)
+    trajectory_plotter(y, key)
 
 if __name__ == "__main__":
     main()

tp_chaos_generator/utils.py
@@ -1,7 +1,53 @@
+""" Utils """
+from __future__ import division
+from math import copysign, frexp, isinf, isnan, trunc
+
+import struct
 import numpy as np
 import matplotlib.pyplot as plt
 
+NEGATIVE_INFINITY = b'\x00\xfc'
+POSITIVE_INFINITY = b'\x00\x7c'
+POSITIVE_ZERO = b'\x00\x00'
+NEGATIVE_ZERO = b'\x00\x80'
+# exp=2**5-1 and significand non-zero
+EXAMPLE_NAN = struct.pack('<H', (0b11111 << 10) | 1)
+
+def half_precision(f):
+    """Convert Python float to IEEE 754-2008 (binary16) format.
+    """
+    if isnan(f):
+        return EXAMPLE_NAN
+
+    sign = copysign(1, f) < 0
+    if isinf(f):
+        return NEGATIVE_INFINITY if sign else POSITIVE_INFINITY
+
+    #           1bit        10bits             5bits
+    # f = (-1)**sign * (1 + f16 / 2**10) * 2**(e16 - 15)
+    # f = (m * 2)                        * 2**(e - 1)
+    m, e = frexp(f)
+    assert not (isnan(m) or isinf(m))
+    if e == 0 and m == 0:  # zero
+        return NEGATIVE_ZERO if sign else POSITIVE_ZERO
+
+    f16 = trunc((2 * abs(m) - 1) * 2**10)  # XXX round toward zero
+    assert 0 <= f16 < 2**10
+    e16 = e + 14
+    if e16 <= 0:  # subnormal
+        # f = (-1)**sign * fraction / 2**10 * 2**(-14)
+        f16 = int(2**14 * 2**10 * abs(f) + .5)  # XXX round
+        e16 = 0
+    elif e16 >= 0b11111:  # infinite
+        return NEGATIVE_INFINITY if sign else POSITIVE_INFINITY
+    else:
+        # normalized value
+        assert 0b00001 <= e16 < 0b11111, (f, sign, e16, f16)
+
+    return struct.pack('<H', (sign << 15) | (e16 << 10) | f16)
+
 def state_plotter(times, states, fig_num):
+    """ Plotting states of triple pendulum"""
     num_states = np.shape(states)[0]
     num_cols = int(np.ceil(np.sqrt(num_states)))
     num_rows = int(np.ceil(num_states / num_cols))
@@ -12,11 +58,11 @@ def state_plotter(times, states, fig_num):
     for n in range(num_states):
         row = n // num_cols
         col = n % num_cols
-        ax[row][col].plot(times, states[n], 'k.:')
+        ax[row][col].plot(times, states[n], 'k.:', markersize=0.5)
         ax[row][col].set(xlabel='Time',
-                         ylabel='$y_{:0.0f}(t)$'.format(n),
-                         title='$y_{:0.0f}(t)$ vs. Time'.format(n))
-        
+                         ylabel=f'$y_{n:0.0f}(t)$',
+                         title=f'$y_{n:0.0f}(t)$ vs. Time')
+
     for n in range(num_states, num_rows * num_cols):
         fig.delaxes(ax[n // num_cols][n % num_cols])
 
@@ -25,20 +71,30 @@ def state_plotter(times, states, fig_num):
 
     return fig, ax
 
-def plot_state(times, states, state_id):
-    plt.plot(times, states[state_id], '.', markersize=0.5)
-    plt.show()
-
-def plot_trajectory(times, states, params):
-    l1, l2, l3 = params[3:6]
+def trajectory_plotter(states, params: list):
+    """ Plotting trajectory of triple pendulum"""
+    L1, L2, L3 = params[3:6]
     phi1 = states[0]
     phi2 = states[1]
     phi3 = states[2]
-    x1, y1 = l1*np.sin(phi1),l1*np.cos(phi1)
-    x2, y2 = l1*np.sin(phi1)+l2*np.sin(phi2),l1*np.cos(phi1)+l2*np.cos(phi2)
-    x3, y3 = l1*np.sin(phi1)+l2*np.sin(phi2)+l3*np.sin(phi3),l1*np.cos(phi1)+l2*np.cos(phi2)+l3*np.cos(phi3)
+    x1, y1 = L1*np.sin(phi1), L1*np.cos(phi1)
+    x2, y2 = L1*np.sin(phi1) + L2*np.sin(phi2), L1*np.cos(phi1) + L2*np.cos(phi2)
+    x3, y3 = L1*np.sin(phi1) + L2*np.sin(phi2) + L3*np.sin(phi3), L1*np.cos(phi1) + L2*np.cos(phi2) + L3*np.cos(phi3)
     plt.plot(x1, y1, '.', markersize=0.5)
     plt.plot(x2, y2, '.', markersize=0.5)
     plt.plot(x3, y3, '.', markersize=0.5)
 
     plt.show()
+
+
+def circular_bit_rotate(n, n_bits, bit_len):
+    """ Circular Bit Rotate """
+    bit_str = bin(n)[2:].zfill(bit_len)
+
+    if n_bits > 0:
+        n_rotated = bit_str[bit_len-n_bits : ] + bit_str[0 : bit_len-n_bits]
+    else:
+        n_bits = -n_bits
+        n_rotated = bit_str[n_bits:] + bit_str[0 : n_bits]
+
+    return int(n_rotated, 2)